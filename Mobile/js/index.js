cdb3af6d4b2de0c65f68a2d52c9de524102c6aba
SumraK327 <90415347+SumraK327@users.noreply.github.com>
2026-08-08 19:32:41 +0300

    Add files via upload

--- Mobile/js/index.js
+++ Mobile/js/index.js
@@ -8,23 +8,23 @@ try {
     HEADER_FIXED: "header--fixed",
   });
 
-  // new BurgerMenu(
-  //   {
-  //     BURGER: "burger",
-  //     BURGER_OPEN: "burger--open",
-  //     HEADER_MENU: "header__menu",
-  //     HEADER_MENU_OPEN: "header__menu--open",
-  //     lABEL: {
-  //       OPEN: "Открыть меню",
-  //       CLOSE: "Закрыть меню",
-  //     },
-  //     PAGE_BODY: "page__body",
-  //     PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
-  //     MENU_LINK: "menu__link",
-  //     BREAKPOINT: 768,
-  //   },
-  //   headerFixed,
-  // );
+  new BurgerMenu(
+    {
+      BURGER: "burger",
+      BURGER_OPEN: "burger--open",
+      HEADER_MENU: "header__menu",
+      HEADER_MENU_OPEN: "header__menu--open",
+      lABEL: {
+        OPEN: "Открыть меню",
+        CLOSE: "Закрыть меню",
+      },
+      PAGE_BODY: "body",
+      PAGE_BODY_NO_SCROLL: "body--no-scroll",
+      MENU_LINK: "menu__link",
+      BREAKPOINT: 768,
+    },
+    headerFixed,
+  );
 
   fetch("intro-swiper.html")
     .then((r) => r.text())
