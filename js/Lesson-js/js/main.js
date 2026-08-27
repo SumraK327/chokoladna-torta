cdd939af26ab33a9d87af6063eeed9b35232e79d
SumraK327 <Tigrubiyca@mail.ru>
2026-08-28 02:03:06 +0300

    1

---
+++ js/Lesson-js/js/main.js
@@ -0,0 +1,12 @@
+let firstName = "Vasay";
+let lastName = "Pupkin";
+let isStudent = true;
+let age = 37;
+let currentYear = 2025;
+let birthYear = currentYear - age;
+
+console.log(
+  `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}.`,
+);
+
+console.log(`${lastName} ${birthYear} года рождения`);
