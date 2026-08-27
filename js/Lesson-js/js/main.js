2599011d63ddef5e20157e28665b3ae8520bc852
SumraK327 <Tigrubiyca@mail.ru>
2026-08-28 02:25:22 +0300

    2

--- js/Lesson-js/js/main.js
+++ js/Lesson-js/js/main.js
@@ -1,12 +1,15 @@
 let firstName = "Vasay";
 let lastName = "Pupkin";
 let isStudent = true;
-let age = 37;
-let currentYear = 2025;
+let age = 38;
+let currentYear = 2026;
 let birthYear = currentYear - age;
+let loh = "Четветую задачу не смог решить :)";
 
 console.log(
   `Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}.`,
 );
 
 console.log(`${lastName} ${birthYear} года рождения`);
+
+console.log(loh);
